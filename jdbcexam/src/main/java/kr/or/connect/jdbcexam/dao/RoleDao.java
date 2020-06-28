daee70834f049889e131b39537d066f0a2ba5caf
LURKS02 <thecrooks2@gmail.com>
2020-06-28 14:47:12 +0900

    db connection

---
+++ jdbcexam/src/main/java/kr/or/connect/jdbcexam/dao/RoleDao.java
@@ -0,0 +1,76 @@
+package kr.or.connect.jdbcexam.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import kr.or.connect.jdbcexam.dto.Role;
+
+public class RoleDao {
+	
+	private static String dburl = "jdbc:mysql://localhost:3306/connectdb";
+	private static String dbUser = "connectuser";
+	private static String dbpasswd = "connect123!@#";
+
+	public Role getRole(Integer roleId) {
+		Role role = null;
+		Connection conn = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(dburl, dbUser, dbpasswd);
+			/*
+			 * 쿼리문에서 변경되는 값은 ?로 표현
+			 * 바인딩된 물음표 값을 바꿔주는 작업이 필요
+			 */
+			String sql = "SELECT description,role_id FROM role WHERE role_id = ?";
+			ps = conn.prepareStatement(sql);
+			ps.setInt(1, roleId);
+			rs = ps.executeQuery();
+			
+			/*
+			 * 결과값이 있다면 첫번째 레코드로 코드 이동
+			 * 다음 호출시 다음 레코드로 이동
+			 * 결과값이 있다면 true 리턴
+			 * 없다면 false 리턴
+			 */
+			if (rs.next()) {
+				String description = rs.getString(1);
+				int id = rs.getInt("role_id");
+				role = new Role(id, description);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			/* 오류가 발생하는 경로가 다양하므로 예외처리 */
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		return role;
+	}
+	
+}
