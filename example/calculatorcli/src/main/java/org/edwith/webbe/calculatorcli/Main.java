4c7009cbd70ac793b17304c7c8ffda387a494d98
LURKS02 <thecrooks2@gmail.com>
2020-09-17 21:10:21 +0900

    spring bean text

---
+++ example/calculatorcli/src/main/java/org/edwith/webbe/calculatorcli/Main.java
@@ -0,0 +1,16 @@
+package org.edwith.webbe.calculatorcli;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+		
+		CalculatorService calculatorService = applicationContext.getBean(CalculatorService.class);
+		System.out.println(calculatorService.plus(10, 50));
+	}
+
+}
