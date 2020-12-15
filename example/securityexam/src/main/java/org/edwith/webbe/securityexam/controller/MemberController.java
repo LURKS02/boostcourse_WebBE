4e39a6d015639eab07c02494d0eeb09cb454e18d
LURKS02 <thecrooks2@gmail.com>
2020-12-15 15:52:37 +0900

    login info

--- example/securityexam/src/main/java/org/edwith/webbe/securityexam/controller/MemberController.java
+++ example/securityexam/src/main/java/org/edwith/webbe/securityexam/controller/MemberController.java
@@ -1,7 +1,12 @@
 package org.edwith.webbe.securityexam.controller;
 
+import java.security.Principal;
+
+import org.edwith.webbe.securityexam.dto.Member;
 import org.edwith.webbe.securityexam.service.MemberService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,9 +17,11 @@ public class MemberController {
 
 	// 스프링 컨테이너가 생성자를 통해 자동으로 주입한다.
     private final MemberService memberService;
+    private final PasswordEncoder passwordEncoder;
 
-    public MemberController(MemberService memberService){
+    public MemberController(MemberService memberService, PasswordEncoder passwordEncoder){
         this.memberService = memberService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @GetMapping("/loginform")
@@ -25,5 +32,32 @@ public class MemberController {
     @RequestMapping("/loginerror")
     public String loginerror(@RequestParam("login_error")String loginError){
         return "members/loginerror";
+    }
+    
+    @GetMapping("/joinform")
+    public String joinform() {
+    	return "members/joinform";
+    }
+    
+    // 사용자가 입력한 name, email, password가 member에 저장된다.
+    /*@PostMapping("/join")
+    public String join(@ModelAttribute Member member){
+        member.setPassword(passwordEncoder.encode(member.getPassword()));
+        memberService.addMember(member, false);
+        return "redirect:/members/welcome";
+    }*/
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "members/welcome";
+    }
+
+    @GetMapping("/memberinfo")
+    public String memberInfo(Principal principal, ModelMap modelMap){
+        String loginId = principal.getName();
+        Member member = memberService.getMemberByEmail(loginId);
+        modelMap.addAttribute("member", member);
+
+        return "members/memberinfo";
     }
 }
--- example/securityexam/src/main/java/org/edwith/webbe/securityexam/service/MemberService.java
+++ example/securityexam/src/main/java/org/edwith/webbe/securityexam/service/MemberService.java
@@ -1,7 +1,15 @@
 package org.edwith.webbe.securityexam.service;
 
+import java.util.List;
+
+import org.edwith.webbe.securityexam.dto.Member;
 import org.edwith.webbe.securityexam.service.security.UserDbService;
+import org.edwith.webbe.securityexam.service.security.UserEntity;
+import org.edwith.webbe.securityexam.service.security.UserRoleEntity;
 
 public interface MemberService extends UserDbService {
-
+	public UserEntity getUser(String loginUserId);
+	public List<UserRoleEntity> getUserRoles(String loginUserId);
+	public Member getMemberByEmail(String loginId);
+	//public void addMember(Member member, boolean bool);
 }
